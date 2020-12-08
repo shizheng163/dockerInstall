b4fdbce515b523bff0aa52760f215d6e6e7a3f0d
zhengshi <zhengshi@autox.ai>
2020-12-08 19:06:45 +0800

    update docker volume path, and with debug mode run.

--- docker_start.sh
+++ docker_start.sh
@@ -1,17 +1,19 @@
 #!/bin/bash
-DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")/../" && pwd )"
-NAME="nodejs-mytest"
+DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")/" && pwd )"
+NAME="mywork"
 IMAGE="shizheng163/ubuntu18.04-self:latest"
 VOLUME_PATH=$DIR
-VOLUME_DOCKER_PATH="/mytest"
+VOLUME_DOCKER_PATH="/work"
 
 echo "Use image:$IMAGE"
 
 set -e
 
-docker run -it \
+docker run -it -d \
           -u $USER \
           --name $NAME \
           -v "${VOLUME_PATH}:${VOLUME_DOCKER_PATH}" \
           -w "${VOLUME_DOCKER_PATH}" \
           $IMAGE
+
+bash docker_into.sh
